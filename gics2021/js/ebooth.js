7450433847b80580107e62bac16239e4058ee0fc
Sumin Lee <sumin416@naver.com>
2021-05-28 07:44:13 +0900

    ebooth position fixed

--- gics2021/js/ebooth.js
+++ gics2021/js/ebooth.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $('.multi-button').on('click', 'button', function() {
-  $(this).addClass('active').siblings().removeClass('active');
-});
+$(document).ready(function () {
+  $('.multi-button').on('click', 'button', function () {
+    $(this).addClass('active').siblings().removeClass('active');
+  });
 });
