470114ba08349a218a7aa03507a92ee551f2d43e
Sumin Lee <sumin416@naver.com>
2021-05-28 20:56:23 +0900

    eposter detail implementing

--- gics2021/js/eposter.js
+++ gics2021/js/eposter.js
@@ -1,3 +1,17 @@
-var cardjs = document.createElement('script');
-cardjs.src = '/gics2021/js/_cards.js';
-document.body.appendChild(cardjs)
+$(document).ready(function(){
+    var like_count = 9;
+    $("#like-count").html(like_count);
+});
+const like = document.querySelector('.like-button')
+
+like.addEventListener('click', () => {
+    var like_count = parseInt(document.getElementById("like-count").innerHTML, 10);
+    if (like.classList.contains('liked')){
+        document.getElementById("like-count").innerHTML = like_count-1;
+        console.log("sub");
+    }else{
+        document.getElementById("like-count").innerHTML = like_count+1;
+        console.log("add");
+    }
+    like.classList.toggle('liked');
+})
