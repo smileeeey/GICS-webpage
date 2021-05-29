8db0a29fbe53870a0d29580d213095d1c2a7fab4
Sumin Lee <sumin416@naver.com>
2021-05-29 18:26:08 +0900

    modal context ready

--- gics2021/js/ebooth.js
+++ gics2021/js/ebooth.js
@@ -2,17 +2,18 @@
 
 $(document).ready(function () {
   $('.multi-button').on('click', 'a', function () {   
+    console.log(window.scrollY)
     $(this.children).addClass('active');
     $(this).siblings().find('button').removeClass('active');
 
     tabid = this.id.split('-').slice(0,2).join('-');
     document.getElementById(tabid).checked = true;
 
-    $("body").addClass("not_scroll");
+    // $("body").addClass("not_scroll");
   });
   
   $("#close_btn").click(function(){
-    $("body").removeClass("not_scroll");
+    // $("body").removeClass("not_scroll");
   });
 
   $(".tabs input").click(function () {
