40a18752f0620a57bdcbec4b2bb818333dd28340
sumin416 <sumin416@naver.com>
2021-05-29 16:26:05 +0900

    booth 전체 수정

--- gics2021/js/ebooth.js
+++ gics2021/js/ebooth.js
@@ -1,5 +1,9 @@
+// https://codepen.io/summin/pen/NWpaegG
+
 $(document).ready(function () {
-  $('.multi-button').on('click', 'button', function () {
-    $(this).addClass('active').siblings().removeClass('active');
+  $('.multi-button').on('click', 'a', function () {
+    // $(this).addClass('active').siblings().removeClass('active');
+    $(this.children).addClass('active');
+    $(this).siblings().find('button').removeClass('active');
   });
 });
