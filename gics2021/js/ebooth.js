4bdb2f8f1fed979701d920f7b165ac8d360e681b
Sumin Lee <sumin416@naver.com>
2021-05-29 18:11:43 +0900

    modal intergrated

--- gics2021/js/ebooth.js
+++ gics2021/js/ebooth.js
@@ -1,9 +1,28 @@
 // https://codepen.io/summin/pen/NWpaegG
 
 $(document).ready(function () {
-  $('.multi-button').on('click', 'a', function () {
-    // $(this).addClass('active').siblings().removeClass('active');
+  $('.multi-button').on('click', 'a', function () {   
     $(this.children).addClass('active');
     $(this).siblings().find('button').removeClass('active');
+
+    tabid = this.id.split('-').slice(0,2).join('-');
+    document.getElementById(tabid).checked = true;
+
+    $("body").addClass("not_scroll");
   });
+  
+  $("#close_btn").click(function(){
+    $("body").removeClass("not_scroll");
+  });
+
+  $(".tabs input").click(function () {
+    btnid = this.id + "-btn"
+    active_btn = document.getElementById(btnid);
+
+    sel = '#'+btnid
+    $(sel).find('button').toggleClass("active")
+    $(sel).siblings().find('button').removeClass('active');
+  })
+
 });
+
