4bca17c1ba635fe22151d02173415d4ca0baef8e
chae-yc <yunchang.chae@gamil.com>
2021-06-05 23:10:05 +0900

    header activation

--- gics2021/js/_components/header.js
+++ gics2021/js/_components/header.js
@@ -1,4 +1,35 @@
 $(document).ready(function () {
+    var curUrl = window.location.pathname;
+    var parsed_url = curUrl.split('/');
+
+    if (parsed_url[3] === undefined) {
+        document.querySelector(".header-item").classList.add('active-header');
+    }else{
+        var dir = parsed_url[2];
+        switch(dir){
+            case "live":
+                document.querySelectorAll(".header-item")[1].classList.add('active-header');
+                break;
+            case "vod":
+                document.querySelectorAll(".header-item")[2].classList.add('active-header');
+                break;
+            case "eposter":
+                document.querySelectorAll(".header-item")[3].classList.add('active-header');
+                break;
+            case "exhibition":
+            case "ebooth":
+                document.querySelectorAll(".header-item")[4].classList.add('active-header');
+                break;
+            case "awards":
+                document.querySelectorAll(".header-item")[5].classList.add('active-header');
+                break;
+            case "history":
+                document.querySelectorAll(".header-item")[6].classList.add('active-header');
+                break;
+        }        
+    }
+    
+    
     $(".header-menu").show();
 });
 
